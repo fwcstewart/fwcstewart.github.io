3bfc041dbf230ff328d7dd0ed4deeb0727f20792
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-15 18:13:12 +0000

    Update script.js

--- rps/resources/js/script.js
+++ rps/resources/js/script.js
@@ -151,15 +151,5 @@ gameImages.forEach(image => {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const burgerButton = document.querySelector('.burger-menu-button');
-    const navElements = document.querySelector('.nav-elements');
-
-    burgerButton.addEventListener('click', function() {
-        this.classList.toggle('is-active');
-        navElements.classList.toggle('is-active');
-    });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
     document.querySelector(".close-button").addEventListener("click", closeModal);
 });
