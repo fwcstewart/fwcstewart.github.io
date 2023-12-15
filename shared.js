613db54f3fed659e087dbe46895bc4e63ac714e2
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-15 18:08:17 +0000

    Update shared.js

--- shared.js
+++ shared.js
@@ -1,1 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const burgerButton = document.querySelector('.burger-menu-button');
+        const navElements = document.querySelector('.nav-elements');
 
+        burgerButton.addEventListener('click', function() {
+            this.classList.toggle('is-active');
+            navElements.classList.toggle('is-active');
+        });
+    });
