c7f0fb40f37f02b29313f9c037bfce86032067b7
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-15 16:57:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Rock Paper Scissors was clicked');
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const playButtons = document.querySelectorAll('.play-btn');
+
+    playButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.target.classList.add('bounce');
+            setTimeout(() => event.target.classList.remove('bounce'), 1000);
+        });
+    });
+});
+
