ece42f2eefa770ef9ec7c774f4b8665e37dd3dab
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-15 18:07:24 +0000

    Update script.js

--- numble/resources/js/script.js
+++ numble/resources/js/script.js
@@ -196,13 +196,3 @@ document.getElementById('closeModalButton').addEventListener('click', () => {
 });
 
 startNewGame();
-
-document.addEventListener('DOMContentLoaded', function() {
-        const burgerButton = document.querySelector('.burger-menu-button');
-        const navElements = document.querySelector('.nav-elements');
-
-        burgerButton.addEventListener('click', function() {
-            this.classList.toggle('is-active');
-            navElements.classList.toggle('is-active');
-        });
-    });
