86e75b7226358fcb7fb1a0ff16ac386c6bb5ebf2
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-18 18:09:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const numbleLink = document.getElementById('numble-link');
+    const rpsLink = document.getElementById('rps-link');
+
+    // Implement any JavaScript interactions or effects you want to apply to the links
+    numbleLink.addEventListener('click', (event) => {
+        // You can add logic before redirecting or animate the transition
+        console.log('Numble was clicked');
+    });
+
+    rpsLink.addEventListener('click', (event) => {
+        // Similar logic for Rock Paper Scissors
+        console.log('Rock Paper Scissors was clicked');
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
     const playButtons = document.querySelectorAll('.play-btn');
 
     playButtons.forEach(button => {
