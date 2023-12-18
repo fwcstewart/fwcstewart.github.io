03262d73cf5d4a151cbc0ac75d9dfffb59fbfc2f
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-18 12:51:28 +0000

    Update shared.js

--- shared.js
+++ shared.js
@@ -7,3 +7,24 @@ document.addEventListener('DOMContentLoaded', function() {
             navElements.classList.toggle('is-active');
         });
     });
+
+document.addEventListener("DOMContentLoaded", function() {
+    var navElement = document.querySelector('.nav-elements nav');
+
+    var links = [
+        { href: "https://numble-game.com", text: "Home" },
+        { href: "https://numble-game.com/numble", text: "Numble" },
+        { href: "https://numble-game.com/rps", text: "RPS" },
+        { href: "https://numble-game.com/highlow", text: "Higher or Lower" },
+        // Add more links as needed
+    ];
+
+    links.forEach(link => {
+        var a = document.createElement('a');
+        a.href = link.href;
+        a.textContent = link.text;
+        a.className = 'nav-item';
+        navElement.appendChild(a);
+    });
+});
+
