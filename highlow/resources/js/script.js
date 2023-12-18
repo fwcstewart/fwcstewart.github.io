c7b33d442466d94e52c6f5126bd53ab09a0ba865
fwcstewart <148849526+fwcstewart@users.noreply.github.com>
2023-12-18 16:26:08 +0000

    Update script.js

--- highlow/resources/js/script.js
+++ highlow/resources/js/script.js
@@ -1,185 +1,104 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Game Element References
-    const currentNumberDisplay = document.getElementById("currentNumber");
-    const nextNumberDisplay = document.getElementById("nextNumber");
-    const higherBtn = document.getElementById("higherBtn");
-    const lowerBtn = document.getElementById("lowerBtn");
-    const scoreDisplay = document.getElementById("score");
-    const streakDisplay = document.getElementById("streak");
-    const levelDisplay = document.getElementById("level");
-    const timeLeftDisplay = document.getElementById("timeLeft");
-    const gameModeButtons = document.querySelectorAll(".mode-btn");
-    const doublePointsBtn = document.getElementById("doublePointsBtn");
-    const feedbackElement = document.getElementById('feedback');
-    const tutorialModal = document.getElementById('tutorialModal');
-    const tutorialText = document.getElementById('tutorialText');
-    const nextTutorialStepBtn = document.getElementById('nextTutorialStep');
+    const elements = {
+        currentNumberDisplay: document.getElementById("currentNumber"),
+        nextNumberDisplay: document.getElementById("nextNumber"),
+        higherBtn: document.getElementById("higherBtn"),
+        lowerBtn: document.getElementById("lowerBtn"),
+        scoreDisplay: document.getElementById("score"),
+        streakDisplay: document.getElementById("streak"),
+        levelDisplay: document.getElementById("level"),
+        timeLeftDisplay: document.getElementById("timeLeft"),
+        gameModeButtons: document.querySelectorAll(".mode-btn"),
+        doublePointsBtn: document.getElementById("doublePointsBtn"),
+        feedbackElement: document.getElementById('feedback'),
+        tutorialModal: document.getElementById('tutorialModal'),
+        tutorialText: document.getElementById('tutorialText'),
+        nextTutorialStepBtn: document.getElementById('nextTutorialStep'),
+        leaderboardSection: document.getElementById('leaderboard-section'),
+        leaderboardList: document.getElementById('leaderboard-list'),
+        closeLeaderboardBtn: document.getElementById('closeLeaderboardBtn'),
+        backBtn: document.getElementById("backBtn")
+    };
 
-    // Game State Variables
-    let currentNumber, nextNumber, score = 0, streak = 0, level = 1, gameTime = 60, timer;
-    let gameMode = "classic";
-    let doublePoints = false;
-    let doublePointsRemaining = 1;
-    let skipsRemaining = 1;
-    let multiplier = 1;
-    let currentTutorialStep = 0;
+    let gameState = {
+        currentNumber: 0,
+        nextNumber: 0,
+        score: 0,
+        streak: 0,
+        level: 1,
+        gameTime: 60,
+        timer: null,
+        gameMode: "classic",
+        doublePoints: false,
+        doublePointsRemaining: 1,
+        skipsRemaining: 1,
+        multiplier: 1,
+        currentTutorialStep: 0,
+        tutorialSteps: [
+            "This is the game area. Here you'll see the current number and make your guesses.",
+            "Click 'Higher' or 'Lower' to make your guess based on whether you think the next number will be higher or lower.",
+            "Your score and level are shown here. Your score increases with each correct guess.",
+            "Use power-ups like 'Skip' or 'Double Points' to help you advance in the game."
+        ]
+    };
 
-    // Tutorial Steps
-    const tutorialSteps = [
-        "This is the game area. Here you'll see the current number and make your guesses.",
-        "Click 'Higher' or 'Lower' to make your guess based on whether you think the next number will be higher or lower.",
-        "Your score and level are shown here. Your score increases with each correct guess.",
-        "Use power-ups like 'Skip' or 'Double Points' to help you advance in the game."
-    ];
-
-    // Update Tutorial
     function updateTutorial(step) {
-        if (step < tutorialSteps.length) {
-            tutorialText.innerText = tutorialSteps[step];
-            tutorialModal.style.display = 'block';
+        if (step < gameState.tutorialSteps.length) {
+            elements.tutorialText.innerText = gameState.tutorialSteps[step];
+            elements.tutorialModal.style.display = 'block';
         } else {
-            tutorialModal.style.display = 'none';
+            elements.tutorialModal.style.display = 'none';
         }
     }
 
-    // Event Listener for Next Tutorial Step
-    nextTutorialStepBtn.addEventListener('click', function() {
-        currentTutorialStep++;
-        updateTutorial(currentTutorialStep);
-    });
-
-    // Double Points Button Event
-    if (doublePointsBtn) {
-        doublePointsBtn.addEventListener("click", useDoublePoints);
-    }
-
-    // Use Skip Function
-    function useSkip() {
-        if (skipsRemaining > 0) {
-            skipsRemaining--;
-            document.getElementById('skipsRemaining').textContent = skipsRemaining;
-            updateGame();
-        }
-    }
-
-    // Update Scoreboard
     function updateScoreboard() {
-        scoreDisplay.textContent = score;
-        streakDisplay.textContent = streak;
-        levelDisplay.textContent = level;
+        elements.scoreDisplay.textContent = gameState.score;
+        elements.streakDisplay.textContent = gameState.streak;
+        elements.levelDisplay.textContent = gameState.level;
     }
 
-    // Use Double Points Function
-    function useDoublePoints() {
-        if (doublePointsRemaining > 0) {
-            doublePoints = true;
-            doublePointsRemaining--;
-            doublePointsBtn.textContent = `Double Points (Remaining: ${doublePointsRemaining})`;
-        }
-    }
-
-    // Update Multiplier
-    function updateMultiplier(isCorrect) {
-        multiplier = isCorrect ? multiplier + 1 : 1;
-    }
-
-    // Update Score
-    function updateScore(isCorrect) {
-        if (isCorrect) {
-            score += 1 * multiplier * (doublePoints ? 2 : 1);
-            doublePoints = false;
-            updateScoreboard();
-        }
-    }
-
-    // Update Leaderboard
- function updateLeaderboard(entry) {
-    const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    entry.gameMode = gameMode; // Add game mode to the entry
-    leaderboard.push(entry);
-    leaderboard.sort((a, b) => b.score - a.score);
-    localStorage.setItem('leaderboard', JSON.stringify(leaderboard.slice(0, 10))); // Increase size if needed
-}
-
-function showLeaderboard() {
-    const leaderboard = JSON.parse(localStorage.getItem('leaderboard')) || [];
-    const leaderboardList = document.getElementById('leaderboard-list');
-    leaderboardList.innerHTML = '';
-    leaderboard.forEach(entry => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `${entry.name}: ${entry.score} (Mode: ${entry.gameMode})`; // Show game mode
-        leaderboardList.appendChild(listItem);
-    });
-        document.getElementById('leaderboard-section').style.display = 'block';
-        document.getElementById('game-interface').style.display = 'none';
-        document.getElementById('game-mode-selection').style.display = 'none';
-    }
-
-    // Close Leaderboard and Return to Start Screen
-    document.getElementById('closeLeaderboardBtn').addEventListener('click', function() {
-        document.getElementById('leaderboard-section').style.display = 'none';
-        document.getElementById('game-mode-selection').style.display = 'block';
-    });
-
-    // Show Feedback
-    function showFeedback(isCorrect, message = '') {
-        feedbackElement.textContent = message || (isCorrect ? 'Correct!' : 'Wrong!');
-        feedbackElement.style.color = isCorrect ? 'green' : 'red';
-        setTimeout(() => {
-            feedbackElement.textContent = '';
-        }, 2000);
-    }
-
-    // Generate Random Number
-    function generateNumber() {
-        return Math.floor(Math.random() * (10 * level)) + 1;
-    }
-
-    // Update Game
     function updateGame() {
-        currentNumber = generateNumber();
-        nextNumber = generateNumber();
-        currentNumberDisplay.textContent = currentNumber;
-        nextNumberDisplay.textContent = "?";
-        scoreDisplay.textContent = score;
-        streakDisplay.textContent = streak;
-        levelDisplay.textContent = level;
-        timeLeftDisplay.textContent = gameTime;
-        document.getElementById('skipsRemaining').textContent = skipsRemaining;
+        gameState.currentNumber = generateNumber();
+        gameState.nextNumber = generateNumber();
+        elements.currentNumberDisplay.textContent = gameState.currentNumber;
+        elements.nextNumberDisplay.textContent = "?";
+        updateScoreboard();
+        elements.timeLeftDisplay.textContent = gameState.gameTime;
+        document.getElementById('skipsRemaining').textContent = gameState.skipsRemaining;
     }
 
     // Handle Guess
-    function handleGuess(isHigher) {
-        const isCorrect = (isHigher && nextNumber > currentNumber) || (!isHigher && nextNumber < currentNumber);
-        showFeedback(isCorrect);
-        nextNumberDisplay.textContent = nextNumber;
-        updateMultiplier(isCorrect);
-        updateScore(isCorrect);
-        if (isCorrect) {
-            streak++;
-            level = Math.floor(score / 10) + 1;
-        } else {
-            if (gameMode === "suddenDeath") {
-                alert("Wrong guess. Game over!");
-                gameOver();
-                return;
-            }
-            streak = 0;
+function handleGuess(isHigher) {
+    const isCorrect = (isHigher && gameState.nextNumber > gameState.currentNumber) || (!isHigher && gameState.nextNumber < gameState.currentNumber);
+    showFeedback(isCorrect);
+    elements.nextNumberDisplay.textContent = gameState.nextNumber;
+    updateMultiplier(isCorrect);
+    updateScore(isCorrect);
+    if (isCorrect) {
+        gameState.streak++;
+        gameState.level = Math.floor(gameState.score / 10) + 1;
+    } else {
+        gameState.streak = 0;
+        if (gameState.gameMode === "suddenDeath") {
+            alert("Wrong guess. Game over!");
+            gameOver();
+            return;
         }
-        setTimeout(updateGame, 2000);
     }
+    setTimeout(updateGame, 2000);
+}
 
-    // Start Timer
-    function startTimer() {
-        timer = setInterval(() => {
-            gameTime--;
-            timeLeftDisplay.textContent = gameTime;
-            if (gameTime <= 0) {
-                clearInterval(timer);
-                gameOver();
-            }
-        }, 1000);
-    }
+// Start Timer
+function startTimer() {
+    gameState.timer = setInterval(() => {
+        gameState.gameTime--;
+        elements.timeLeftDisplay.textContent = gameState.gameTime;
+        if (gameState.gameTime <= 0) {
+            clearInterval(gameState.timer);
+            gameOver();
+        }
+    }, 1000);
+}
 
     // Start Game
     function startGame(selectedMode) {
@@ -216,23 +135,88 @@ function showLeaderboard() {
         resetGame();
     }
 
-    // Event Listeners for Game Control
-    higherBtn.addEventListener("click", () => handleGuess(true));
-    lowerBtn.addEventListener("click", () => handleGuess(false));
-    gameModeButtons.forEach(btn => {
-        btn.addEventListener("click", () => {
-            const mode = btn.getAttribute("data-mode");
-            document.querySelectorAll(".game-section").forEach(el => el.style.display = "block");
-            document.getElementById("game-mode-selection").style.display = "none";
-            startGame(mode);
+    // Show Feedback
+    function showFeedback(isCorrect, message = '') {
+        feedbackElement.textContent = message || (isCorrect ? 'Correct!' : 'Wrong!');
+        feedbackElement.style.color = isCorrect ? 'green' : 'red';
+        setTimeout(() => {
+            feedbackElement.textContent = '';
+        }, 2000);
+    }
+
+      // Attaching all event listeners
+    attachEventListeners();
+
+    function attachEventListeners() {
+        elements.nextTutorialStepBtn.addEventListener('click', () => {
+            gameState.currentTutorialStep++;
+            updateTutorial(gameState.currentTutorialStep);
         });
+
+        elements.doublePointsBtn.addEventListener("click", useDoublePoints);
+        elements.closeLeaderboardBtn.addEventListener('click', closeLeaderboard);
+        elements.backBtn.addEventListener("click", backToMenu);
+
+        elements.gameModeButtons.forEach(btn => {
+            btn.addEventListener("click", () => selectGameMode(btn));
+        });
+
+        elements.higherBtn.addEventListener("click", () => handleGuess(true));
+        elements.lowerBtn.addEventListener("click", () => handleGuess(false));
+    }
+
+    function selectGameMode(btn) {
+        const mode = btn.getAttribute("data-mode");
+        document.querySelectorAll(".game-section").forEach(el => el.style.display = "block");
+        elements.gameModeSelection.style.display = "none";
+        startGame(mode);
+    }
+
+    function closeLeaderboard() {
+        elements.leaderboardSection.style.display = 'none';
+        elements.gameModeSelection.style.display = 'block';
+    }
+
+    function backToMenu() {
+        elements.gameInterface.style.display = "none";
+        elements.gameModeSelection.style.display = "block";
+    }
+
+  // Utility function to generate a random number based on the current level
+function generateNumber() {
+    return Math.floor(Math.random() * (10 * gameState.level)) + 1;
+}
+
+// Utility function to handle the double points power-up
+function useDoublePoints() {
+    if (gameState.doublePointsRemaining > 0) {
+        gameState.doublePoints = true;
+        gameState.doublePointsRemaining--;
+        elements.doublePointsBtn.textContent = `Double Points (Remaining: ${gameState.doublePointsRemaining})`;
+    }
+}
+
+// Utility function to update the score multiplier
+function updateMultiplier(isCorrect) {
+    gameState.multiplier = isCorrect ? gameState.multiplier + 1 : 1;
+}
+
+// Utility function to update the score
+function updateScore(isCorrect) {
+    if (isCorrect) {
+        gameState.score += 1 * gameState.multiplier * (gameState.doublePoints ? 2 : 1);
+        gameState.doublePoints = false; // Reset double points after use
+        updateScoreboard();
+    }
+}
+  
+    // Initialise the tutorial
+    updateTutorial(gameState.currentTutorialStep);
+
+    // Initial reset of the game
+    resetGame();
     });
 
-    document.getElementById("backBtn").addEventListener("click", () => {
-        document.getElementById("game-interface").style.display = "none";
-        document.getElementById("game-mode-selection").style.display = "block";
-    });
 
-    // Initialize the tutorial
-    updateTutorial(currentTutorialStep);
-});
+
+
